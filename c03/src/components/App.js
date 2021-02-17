33a87cf9a944010a5c41539e7de0e082f6f49dd1
ivanak <ivana@beyondbasics.co>
2021-02-17 11:57:53 +0100

    c03 - fix array structure problem

--- c03/src/components/App.js
+++ c03/src/components/App.js
@@ -98,7 +98,6 @@ const App = () => {
   );
 };
 
-/*
 let posts = [
   {
     'userId': 1,
@@ -126,69 +125,68 @@ let posts = [
         'name': 'odio adipisci rerum aut animi',
         'email': 'Nikita@garfield.biz',
         'body': 'quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione'
+      }
+    ]
+  },
+  {
+    'userId': 2,
+    'id': 11,
+    'title': 'et ea vero quia laudantium autem',
+    'body': 'delectus reiciendis molestiae occaecati non minima eveniet qui voluptatibus\naccusamus in eum beatae sit\nvel qui neque voluptates ut commodi qui incidunt\nut animi commodi',
+    'comments': [
+      {
+        'postId': 11,
+        'id': 51,
+        'name': 'molestias et odio ut commodi omnis ex',
+        'email': 'Laurie@lincoln.us',
+        'body': 'perferendis omnis esse\nvoluptate sit mollitia sed perferendis\nnemo nostrum qui\nvel quis nisi doloribus animi odio id quas'
       },
       {
-        'userId': 2,
-        'id': 11,
-        'title': 'et ea vero quia laudantium autem',
-        'body': 'delectus reiciendis molestiae occaecati non minima eveniet qui voluptatibus\naccusamus in eum beatae sit\nvel qui neque voluptates ut commodi qui incidunt\nut animi commodi',
-        'comments': [
-          {
-            'postId': 11,
-            'id': 51,
-            'name': 'molestias et odio ut commodi omnis ex',
-            'email': 'Laurie@lincoln.us',
-            'body': 'perferendis omnis esse\nvoluptate sit mollitia sed perferendis\nnemo nostrum qui\nvel quis nisi doloribus animi odio id quas'
-          },
-          {
-            'postId': 11,
-            'id': 52,
-            'name': 'esse autem dolorum',
-            'email': 'Abigail.OConnell@june.org',
-            'body': 'et enim voluptatem totam laudantium\nimpedit nam labore repellendus enim earum aut\nconsectetur mollitia fugit qui repellat expedita sunt\naut fugiat vel illo quos aspernatur ducimus'
-          },
-          {
-            'postId': 11,
-            'id': 53,
-            'name': 'maiores alias necessitatibus aut non',
-            'email': 'Laverne_Price@scotty.info',
-            'body': 'a at tempore\nmolestiae odit qui dolores molestias dolorem et\nlaboriosam repudiandae placeat quisquam\nautem aperiam consectetur maiores laboriosam nostrum'
-          }
-        ]
+        'postId': 11,
+        'id': 52,
+        'name': 'esse autem dolorum',
+        'email': 'Abigail.OConnell@june.org',
+        'body': 'et enim voluptatem totam laudantium\nimpedit nam labore repellendus enim earum aut\nconsectetur mollitia fugit qui repellat expedita sunt\naut fugiat vel illo quos aspernatur ducimus'
       },
       {
-        'userId': 3,
-        'id': 26,
-        'title': 'est et quae odit qui non',
-        'body': 'similique esse doloribus nihil accusamus\nomnis dolorem fuga consequuntur reprehenderit fugit recusandae temporibus\nperspiciatis cum ut laudantium\nomnis aut molestiae vel vero',
-        'comments': [
-          {
-            'postId': 26,
-            'id': 126,
-            'name': 'fuga tenetur id et qui labore delectus',
-            'email': 'Alexie@alayna.org',
-            'body': 'est qui ut tempore temporibus pariatur provident qui consequuntur\nlaboriosam porro dignissimos quos debitis id laborum et totam\naut eius sequi dolor maiores amet\nrerum voluptatibus quod ratione quos labore fuga sit'
-          },
-          {
-            'postId': 26,
-            'id': 127,
-            'name': 'consequatur harum magnam',
-            'email': 'Haven_Barrows@brant.org',
-            'body': 'omnis consequatur dignissimos iure rerum odio\nculpa laudantium quia voluptas enim est nisi\ndoloremque consequatur autem officiis necessitatibus beatae et\net itaque animi dolor praesentium'
-          },
-          {
-            'postId': 26,
-            'id': 128,
-            'name': 'labore architecto quaerat tempora voluptas consequuntur animi',
-            'email': 'Marianne@maximo.us',
-            'body': 'exercitationem eius aut ullam vero\nimpedit similique maiores ea et in culpa possimus omnis\neos labore autem quam repellendus dolores deserunt voluptatem\nnon ullam eos accusamus'
-          }
-        ]
+        'postId': 11,
+        'id': 53,
+        'name': 'maiores alias necessitatibus aut non',
+        'email': 'Laverne_Price@scotty.info',
+        'body': 'a at tempore\nmolestiae odit qui dolores molestias dolorem et\nlaboriosam repudiandae placeat quisquam\nautem aperiam consectetur maiores laboriosam nostrum'
+      }
+    ]
+  },
+  {
+    'userId': 3,
+    'id': 26,
+    'title': 'est et quae odit qui non',
+    'body': 'similique esse doloribus nihil accusamus\nomnis dolorem fuga consequuntur reprehenderit fugit recusandae temporibus\nperspiciatis cum ut laudantium\nomnis aut molestiae vel vero',
+    'comments': [
+      {
+        'postId': 26,
+        'id': 126,
+        'name': 'fuga tenetur id et qui labore delectus',
+        'email': 'Alexie@alayna.org',
+        'body': 'est qui ut tempore temporibus pariatur provident qui consequuntur\nlaboriosam porro dignissimos quos debitis id laborum et totam\naut eius sequi dolor maiores amet\nrerum voluptatibus quod ratione quos labore fuga sit'
+      },
+      {
+        'postId': 26,
+        'id': 127,
+        'name': 'consequatur harum magnam',
+        'email': 'Haven_Barrows@brant.org',
+        'body': 'omnis consequatur dignissimos iure rerum odio\nculpa laudantium quia voluptas enim est nisi\ndoloremque consequatur autem officiis necessitatibus beatae et\net itaque animi dolor praesentium'
+      },
+      {
+        'postId': 26,
+        'id': 128,
+        'name': 'labore architecto quaerat tempora voluptas consequuntur animi',
+        'email': 'Marianne@maximo.us',
+        'body': 'exercitationem eius aut ullam vero\nimpedit similique maiores ea et in culpa possimus omnis\neos labore autem quam repellendus dolores deserunt voluptatem\nnon ullam eos accusamus'
       }
     ]
   }
 ];
-*/
 
 /* Homework 03
   1. Create Posts component and display the data from the given posts array. 
