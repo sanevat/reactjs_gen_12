a1decd40d915aa543d9384946b7b22ab08a45234
ivanak <ivana@beyondbasics.co>
2021-03-03 22:56:19 +0100

    c08 materials

---
+++ c08/src/components/App.js
@@ -0,0 +1,19 @@
+const  App = () => {
+  return (
+    <div className='app'>
+    </div>
+  );
+};
+
+/* Homework 08
+  1. Create simple signup form. The form should be consisted of:
+    * First name - reuqired, minimum length 2 characters, maximum length 15 characters
+    * Last name - reuqired, minimum length 2 characters, maximum length 20 characters
+    * Email - required; (**bonus** check if it is valid email)
+    * Password - required
+    * 'Sign Up' button
+    For all the fields above, display the correct validation messages.
+    2. Finish ToDo list app. 
+*/
+
+export default App;
